282b518b853601ef86f5269a3d7a08a9f3912b57
Avik816 <manikchakrabarti745@gmail.com>
2026-09-02 19:26:56 +0530

    feat(backend): Updated chat route

--- src/backend/api/routes/chat.py
+++ src/backend/api/routes/chat.py
@@ -1,19 +1,17 @@
 from fastapi import APIRouter
 from backend.data_schemas.request import ChatRequest
 from backend.data_schemas.response import ChatResponse
-from backend.services.chat_service import ChatService
+from backend.services.chat_service import process_request
 
 
 
-router = APIRouter()
+router = APIRouter(prefix = '/chat', tags = ['chat'])
 
 @router.post(
-    '/chat',
-    response_model=ChatResponse
+    '/',
+    response_model = ChatResponse
 )
 
 # Handles the incomming chat requests
 async def chat(request: ChatRequest) -> ChatResponse:
-    chat_service = ChatService()
-
-    return chat_service.process_request(request=request)
+    return process_request(request=request)
